c773987dfb2fcadb85bca92ab2a67b714e9db024
Minaszy <583400916@qq.com>
2019-11-09 16:57:12 +0800

    使用cesium绘制地球

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, Directive, OnInit } from '@angular/core';
+declare var Cesium;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage implements OnInit {
+  @ViewChild('map', null) mapContainer: ElementRef;
+  constructor() { }
+  ngOnInit() {
+    const viewer = new Cesium.Viewer(this.mapContainer.nativeElement);
+  }
 }
